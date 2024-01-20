ccfcd707283ec4c4c2c51f95ce3fec3a4dbc85da
marcinsciborek <67116044+marcinsciborek@users.noreply.github.com>
2024-01-20 18:34:20 +0100

    Add aspectjweaver dependency, update allure version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("java")
-    id("io.qameta.allure") version "2.11.2"
+    id("io.qameta.allure") version "2.25.0"
 }
 
 group = "com.confluence"
@@ -15,11 +15,13 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 
     testImplementation("org.assertj:assertj-core:3.25.1")
-    testImplementation("io.qameta.allure:allure-junit5:2.17.3")
+    testImplementation("io.qameta.allure:allure-junit5:2.25.0")
 
     implementation("com.microsoft.playwright:playwright:1.40.0")
     implementation("io.github.uchagani:allure-playwright-java:1.1.0")
     implementation("org.springframework:spring-context:6.1.2")
+
+    runtimeOnly("org.aspectj:aspectjweaver:1.9.21")
 
     compileOnly("org.projectlombok:lombok:1.18.30")
 
